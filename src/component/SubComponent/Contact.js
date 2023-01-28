597e2420f8e29ef4632aa7fd8ba77bf75effb0b1
aniketvs <sharmavinod8454@gmail.com>
2023-01-28 17:20:36 +0530

    after updating contact backend

--- src/component/SubComponent/Contact.js
+++ src/component/SubComponent/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Grid, Typography } from '@mui/material'
 import '../style/Contact.css';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
@@ -10,10 +10,55 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TextField from '@mui/material/TextField';
 import TwitterIcon from '@mui/icons-material/Twitter';
 export default function Contact() {
+    const [name, setname] = useState("");
+    const [email, setemail] = useState("");
+    const [message, setmessage] = useState("");
+    const [err, seterr] = useState(false);
+    const submit = async () => {
+       
+        try {
+            if(!email){
+                seterr(true);
+                return false;
+            }
+            if(!name){
+                seterr(true);
+                return false;
+            }
+            if (!name && !email) {
+               
+                seterr(true);
+                
+                return false;
+            }
+            let result = await fetch('http://localhost:3005/contact', {
+                method: 'POST',
+                body: JSON.stringify({ name, email, message }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            result = await result.json();
+            if (result.name) {
+                alert('Thanks for contacting me');
+                setname("");
+                setemail("");
+                setmessage("");
+            } else {
+                alert("something went wrong");
+            }
+        } catch (error) {
+            console.warn(error);
+        }
+    }
+const change=(e)=>{
+    e.preventDefault();
+    seterr(false);
+}
     return (
         <>
             <Box className="container  mt-5" id="contact">
-                <Grid container  className="Main-grid">
+                <Grid container className="Main-grid">
                     <Grid item xs={12} sm={12} md={5} lg={5} >
                         <Box className="mb-5" data-aos="fade-right">
                             <Typography className='contact-header' variant='h3'>Contact <span className='text-col'>Me. </span> </Typography>
@@ -36,32 +81,70 @@ export default function Contact() {
                     </Grid>
                     <Grid item xs={12} sm={12} md={5} lg={5}>
                         <Box className="mb-5" data-aos="fade-left">
-                            <TextField
-                                required
-                                id="outlined-required"
-                                label="Enter Your Name"
-                                fullWidth={true}
-                                className="text-area"
+                            {!name && err ?
+                                <TextField
+                                  
+                                    required
+                                    value={name}
+                                    onChange={(e) => { setname(e.target.value) }}
+                                    error
+                                    id="outlined-error"
+                                    label="Enter Your Name"
+                                    fullWidth={true}
+                                    className="text-area"
+                                    onClick={change}
 
-                            />
-                            <TextField
-                             className="text-area"
+                                /> :
+                                <TextField
+                                    required
+                                    value={name}
+                                    onChange={(e) => { setname(e.target.value) }}
+                                    id="outlined-required"
+                                    label="Enter Your Name"
+                                    fullWidth={true}
+                                    className="text-area"
+
+                                />
+                            }
+                            {!email && err ? 
+                                <TextField
+                                className="text-area"
                                 required
+                                error
+                                value={email}
+                                id="outlined-error"
+                                    label="Enter Your Email"
+                             
+                                onChange={(e) => { setemail(e.target.value) }}
+                              
+                                fullWidth={true}
+                                onClick={change}
+
+                            />:
+                            <TextField
+                                className="text-area"
+                                required
+                                value={email}
+
+                                onChange={(e) => { setemail(e.target.value) }}
                                 id="outlined-required"
                                 label="Enter Your Email"
                                 fullWidth={true}
 
                             />
+                            }
                             <TextField
-                             className="text-area"
+                                className="text-area"
                                 id="outlined-multiline-static"
                                 label="Message"
                                 multiline
+                                value={message}
+                                onChange={(e) => { setmessage(e.target.value) }}
                                 rows={3}
                                 placeholder="Enter Your Message"
                                 fullWidth={true}
                             />
-                            <Button variant="contained" className="btn-sub"> Submit </Button>
+                            <Button onClick={submit} variant="contained" className="btn-sub"> Submit </Button>
 
                         </Box>
                     </Grid>

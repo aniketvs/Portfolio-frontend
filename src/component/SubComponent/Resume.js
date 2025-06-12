a253f8c0791b82b7020e237b16a0c17529181ecb
Aniket Vinod Sharma <72964172+aniketvs@users.noreply.github.com>
2025-06-13 05:29:23 +0530

    Update Resume.js

--- src/component/SubComponent/Resume.js
+++ src/component/SubComponent/Resume.js
@@ -2,7 +2,7 @@ import { Box, Typography, Button} from '@mui/material'
 import React, { useState } from 'react'
 import '../style/Resume.css'
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
-import File from '../File/Sharma_Aniket_Vinod.pdf'
+import File from '../File/Aniket_Resume_2025.pdf'
 export default function Resume() {
     const [click,setClick]=useState(0);
     const onButtonClick = () => {
